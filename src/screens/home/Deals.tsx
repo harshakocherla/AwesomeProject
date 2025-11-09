c744a6638f02c700e013d0fb03610e4c995db52d
Harsha Kocherla <kocherlaharsha0426@gmail.com>
2025-11-09 17:13:07 -0600

    move between one tab to another

--- src/screens/home/Deals.tsx
+++ src/screens/home/Deals.tsx
@@ -1,16 +1,24 @@
 import { StyleSheet, Text, View } from "react-native"
-import { AppRouteProp, AppRoutes } from "../../navigation/routes.constants"
+import { AppNavigationProp, AppRouteProp, AppRoutes } from "../../navigation/routes.constants"
+import { useNavigation } from '@react-navigation/native'
+import { Button } from "@react-navigation/elements"
 
 type DealsProps = {
     route: AppRouteProp<AppRoutes.DEALS>
 }
 export const Deals = ({ route }: DealsProps) => {
+    const navigation = useNavigation<AppNavigationProp>()
     const { dealId } = route.params
     return (
         <View style={styles.container}>
             <Text>
                 This is Deals: {dealId} Screen
             </Text>
+            <Button onPressIn={() => {
+                navigation.navigate(AppRoutes.PRODUCTS)
+            }}>
+                View all Products
+            </Button>
         </View>
     )
 }
